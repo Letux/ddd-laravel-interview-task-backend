b83f26a097d226ae6909eca122511900f3cdcc5c
Alexey Russkin <alexey.russkin@gmail.com>
2023-11-28 13:16:34 +0100

    Added invites module DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(\App\Modules\Invoices\Infrastructure\Database\Seeders\DatabaseSeeder::class);
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
